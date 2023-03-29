9e620b5f2b2235bba4035056df9ab4cd2997b031
ReplaceU <carter.zhang@foreo.com>
2023-03-29 18:38:25 +0800

    commit

---
+++ src/LeetCode/java/com/carter/structure/GraphTurnOrBreak.java
@@ -0,0 +1,17 @@
+package com.carter.structure;
+
+/**
+ * Jungle 生活在美丽的蓝鲸城，大马路都是方方正正，但是每天马路的封闭情况都不一
+ * 样地图由以下元素组成:
+ * 1)"0"一 空地，可以达到
+ * 2)"1"一 路障，不可达到:
+ * 3)"s"- Jungle 的家
+ * 4)"t"一公司.
+ * 其中我们会限制 Jungle 拐弯的次数，同时 Jungle 可以清除给定个数的路障，现在你的
+ * 任务是计算 Jungle 是否可以从家里出发到达公司。
+ */
+public class GraphTurnOrBreak {
+    public static void main(String[] args) {
+
+    }
+}
